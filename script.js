bf721cc364e124b5ccc517be6dc9605f4433092c
Abhishek Krishna <69499326+MRGorillla@users.noreply.github.com>
2024-08-27 21:12:13 +0530

    pc compatability

--- script.js
+++ script.js
@@ -48,15 +48,21 @@ async function updateCameraList(selectElement, videoElement) {
 
     selectElement.innerHTML = '';
 
-    const frontOption = document.createElement('option');
-    frontOption.value = 'user';
-    frontOption.text = 'Front Camera';
-    selectElement.appendChild(frontOption);
+    // const frontOption = document.createElement('option');
+    // frontOption.value = 'user';
+    // frontOption.text = 'Front Camera';
+    // selectElement.appendChild(frontOption);
 
-    const backOption = document.createElement('option');
-    backOption.value = 'environment';
-    backOption.text = 'Back Camera';
-    selectElement.appendChild(backOption);
+    // const backOption = document.createElement('option');
+    // backOption.value = 'environment';
+    // backOption.text = 'Back Camera';
+    // selectElement.appendChild(backOption);
+    videoDevices.forEach(device => {
+        const option = document.createElement('option');
+        option.value = device.deviceId;
+        option.text = device.label || `Camera ${selectElement.length + 1}`;
+        selectElement.appendChild(option);
+    });
 
     selectElement.addEventListener('change', async () => {
         await startCamera(videoElement, selectElement.value);
