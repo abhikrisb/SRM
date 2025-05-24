8533499118673287772e84ae3d66dc586924b100
Abhishek Krishna <69499326+abhikrisb@users.noreply.github.com>
2025-05-24 17:48:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 const config = {
-    faceApiKey: '463191e316cf48db8f49d398612000bf',
-    faceApiEndpoint: 'https://abhishek1.cognitiveservices.azure.com/',
-    visionApiKey: 'AIzaSyD88FlQ9-fit1sSB9Q7OHp1-YGC0Ov2AJ8'
+    faceApiKey: '',
+    faceApiEndpoint: '',
+    visionApiKey: ''
 };
 
 export default config;
